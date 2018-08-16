580c66d37b6224251e527338f5656553574acec0
Tomasz Goliński <tgolinski55@gmail.com>
2018-08-16 15:25:35 +0200

    Add functionality to projectList on MainWindow

--- HandyTest/MainWindow.xaml.cs
+++ HandyTest/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using HandyTest.BL;
 using HandyTest.Properties;
 using System.IO;
 using System.Web;
+using System.Collections.Generic;
 
 namespace HandyTest
 {
@@ -49,6 +50,21 @@ namespace HandyTest
         {         
             projectsListDataGrid.ItemsSource = ProjectsList;
             ProjectsList.Add(new ProjectList("Hello World!"));          
+            ProjectsList.Add(new ProjectList("Hello World2!"));          
+            ProjectsList.Add(new ProjectList("Hello World3!"));          
+        }
+
+        private void makeActiveProjectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var data in projectsListDataGrid.SelectedItems)
+            {
+                ProjectList myData = data as ProjectList;
+                activeProjectTxtBlock.Text = myData.Name;
+            }
+        }
+
+        private void projectsListDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
         }
     }
 }
