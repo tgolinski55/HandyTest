9cd2a23ba173c973a56a396e9e13c63eca5b4c92
Tomasz Goliński <tgolinski55@gmail.com>
2018-12-28 16:01:36 +0100

    Add simple view to AllIssues edit view

--- HandyTest/Pages/AllIssues.xaml.cs
+++ HandyTest/Pages/AllIssues.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Windows.Media;
 using System.Windows;
 using System.Windows.Controls;
+using System;
 
 namespace HandyTest.Pages
 {
@@ -38,6 +39,35 @@ namespace HandyTest.Pages
                 allIssuesTree.Items.Add(Path.GetFileNameWithoutExtension(count.ToString()+". "+o.Name));
                 count++;
             }
+        }
+
+        private void AddPriorityItems(object sender, RoutedEventArgs e)
+        {
+            setpriorityCombo.Items.Add("Critical");
+            setpriorityCombo.Items.Add("Major");
+            setpriorityCombo.Items.Add("Medium");
+            setpriorityCombo.Items.Add("Minor");
+            setpriorityCombo.Items.Add("Cosmetics");
+        }
+
+        private void AddTypeItems(object sender, RoutedEventArgs e)
+        {
+            setreporttypeCombo.Items.Add("Task");
+            setreporttypeCombo.Items.Add("Bug");
+            setreporttypeCombo.Items.Add("Feature");
+        }
+
+        private void AddStateItems(object sender, RoutedEventArgs e)
+        {
+            setstateCombo.Items.Add("Submitted");
+            setstateCombo.Items.Add("Success");
+            setstateCombo.Items.Add("Failure");
+            setstateCombo.Items.Add("Won't Fix");
+        }
+        private void SetCurrentData()
+        {
+            var currentData = DateTime.Today;
+            setreportDateFile.Text = currentData.ToShortDateString();
         }
     }
 }
