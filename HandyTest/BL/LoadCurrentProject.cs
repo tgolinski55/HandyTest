d2a038768e2627f24d188ae523a1fb8eb38edad2
Shazovi <tgolinski55@gmail.com>
2019-01-09 17:49:51 +0100

    Fix an error when config file does not exist

--- HandyTest/BL/LoadCurrentProject.cs
+++ HandyTest/BL/LoadCurrentProject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,18 +16,33 @@ namespace HandyTest.BL
 
         public string GetCurrentProject()
         {
+            string projectName;
+            if (File.Exists(path))
+            {
             xmlFile.Load(path);
             XmlNodeList xmlNodeList = xmlFile.GetElementsByTagName("Project");
-            string projectName = xmlNodeList.Item(0).InnerText;
+            projectName = xmlNodeList.Item(0).InnerText;
+
+            }
+            else
+            {
+                projectName = "";
+            }
             return projectName;
         }
         public int GetCurrentIndex()
         {
             int x = 0;
+            if (File.Exists(path))
+            {
             xmlFile.Load(path);
             XmlNodeList xmlNodeList = xmlFile.GetElementsByTagName("Id");
             string projectIndex = xmlNodeList.Item(0).InnerText;
             int.TryParse(projectIndex, out x);
+
+            }
+            else
+                x = 0;
             return x;
         }
     }
