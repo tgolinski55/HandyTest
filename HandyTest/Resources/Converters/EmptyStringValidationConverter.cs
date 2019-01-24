ea39b8e6da40e94d82be1de2dc58bb97a8694221
Tomasz Goliński <tgolinski55@gmail.com>
2019-01-24 16:48:50 +0100

    Add empty string validator

---
+++ HandyTest/Resources/Converters/EmptyStringValidationConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HandyTest.Resources.Converters
+{
+    public class EmptyStringValidationConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string text1 = values[0] as string;
+            if (string.IsNullOrEmpty(text1)) return false;
+
+            string text2 = values[0] as string;
+            if (string.IsNullOrEmpty(text2)) return false;
+
+            return true;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
